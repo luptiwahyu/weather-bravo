edf222d32cf0296d3e72bc37353f8d29e67f7c6a
Lupti Wahyu <lupti.utama@advotics.com>
2025-04-16 22:51:40 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const title = ref('Hello Vue!')
+
+setTimeout(() => {
+  title.value = 'Gracias!!'
+}, 2000)
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem temporibus eligendi dicta sit inventore quam
+      nostrum tempore repellat quas a repellendus maxime amet dignissimos explicabo blanditiis, maiores dolore rem
+      consequatur.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+p {
+  color: green;
+}
+</style>
