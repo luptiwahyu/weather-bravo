c25226b1833fbef4608cdadbb1ed70beb098b55e
Lupti Wahyu <lupti.utama@advotics.com>
2025-04-16 23:35:15 +0700

    feat: add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,49 @@ setTimeout(() => {
 
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem temporibus eligendi dicta sit inventore quam
-      nostrum tempore repellat quas a repellendus maxime amet dignissimos explicabo blanditiis, maiores dolore rem
-      consequatur.
-    </p>
+    <div>
+      <h1>{{ title }}</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem temporibus eligendi dicta sit inventore quam
+        nostrum tempore repellat quas a repellendus maxime amet dignissimos explicabo blanditiis, maiores dolore rem
+        consequatur.
+      </p>
+    </div>
+
+    <div class="welcome">
+      <div class="welcome__title">Welcome</div>
+      <div class="welcome__description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam deserunt saepe, rem ad eum, molestiae quae deleniti quam. Eius voluptas incidunt autem assumenda repudiandae sapiente dolorum ipsa nemo distinctio asperiores!
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+@reference '@/assets/styles/main.css';
+
 p {
-  color: green;
+  @apply text-xl text-green-600;
+}
+
+.welcome {
+  width: 500px;
+  background-color: yellow;
+  margin-top: 100px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 30px;
+}
+
+.welcome__title {
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  color: blue;
+}
+
+.welcome__description {
+  color: gray;
+  font-style: italic;
 }
 </style>
