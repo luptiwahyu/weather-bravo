2d22a8ecaba177e1fa3f6813f76f42da5db57674
Lupti Wahyu <lupti.utama@advotics.com>
2025-05-06 16:22:29 +0700

    feat: add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/weather-beta',
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(),
     tailwindcss(),
   ],
   resolve: {
