8454276129392478d6ff83ff89b2da7348e81c7d
Lupti Wahyu <luptiwahyu@ymail.com>
2025-08-19 09:30:04 +0700

    feat: change homepage url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/weather-beta',
+  base: '/weather-bravo',
   plugins: [
     vue(),
     // vueDevTools(),
